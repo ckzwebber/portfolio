7d4edd328f7ab0bfaf96ab2801303a42270e788c
ckzwebber <kacowebber11@outlook.com>
2026-08-26 16:23:35 -0300

    fix: SEO

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -10,7 +10,7 @@ export const personalInfo = {
   title: "Backend Developer",
   location: "Criciúma - SC - Brasil 🇧🇷",
   education: "Ciência da Computação - UNESC (em andamento)",
-  linkedin: "https://www.linkedin.com/in/cakowebber/",
+  linkedin: "https://www.linkedin.com/in/cmiguelwm/",
   github: "https://github.com/ckzwebber",
   portfolio: "https://cakowebber.dev/",
   email: "kacowebber11@unesc.net",
